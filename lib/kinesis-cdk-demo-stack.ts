9776dfbad9bf2becb1ad5b188c671109975cc607
Villalba <lmv@f84d899822fc.ant.amazon.com>
2022-10-12 15:41:03 +0300

    adding kinesis and function

--- lib/kinesis-cdk-demo-stack.ts
+++ lib/kinesis-cdk-demo-stack.ts
@@ -1,16 +1,43 @@
 import * as cdk from 'aws-cdk-lib';
+import { CfnOutput } from 'aws-cdk-lib';
+import { Stream, StreamMode } from 'aws-cdk-lib/aws-kinesis';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
-
+import {
+	Function,
+	Runtime,
+	Code,
+	EventSourceMapping,
+	StartingPosition,
+} from 'aws-cdk-lib/aws-lambda';
+import * as path from 'path';
 export class KinesisCdkDemoStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+		super(scope, id, props);
 
-    // The code that defines your stack goes here
+		const stream = new Stream(this, 'KDS', {
+			streamName: 'my-kinesis-data-stream',
+			streamMode: StreamMode.ON_DEMAND,
+		});
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'KinesisCdkDemoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+		/* Lambda function that consumes messages from stream */
+		const consumer = new Function(this, 'ConsumerFunction', {
+			code: Code.fromAsset(path.join(__dirname, '../functions')),
+			runtime: Runtime.NODEJS_16_X,
+			handler: 'consumer.handler',
+		});
+
+		new EventSourceMapping(this, 'KDSConsumerFunctionEvent', {
+			target: consumer,
+			batchSize: 100,
+			startingPosition: StartingPosition.LATEST,
+			eventSourceArn: stream.streamArn,
+		});
+
+		stream.grantRead(consumer);
+
+		new CfnOutput(this, 'StreamARN', {
+			value: stream.streamArn,
+			description: 'StreamARN',
+		});
+	}
 }
